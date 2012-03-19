8676f2d9b4c533014414cc4c121195c72f48f093
stefan.kraus05 <stefan.kraus05>
2012-03-19 16:55:52 +0000

    add SymbolTabelle.cpp add SymbolTabelle.h

---
+++ SymbolTabelle.cpp
@@ -0,0 +1,18 @@
+/*
+ * SymbolTabelle.cpp
+ *
+ *  Created on: 19.03.2012
+ *      Author: stefan
+ */
+
+#include "SymbolTabelle.h"
+
+SymbolTabelle::SymbolTabelle() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SymbolTabelle::~SymbolTabelle() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ SymbolTabelle.h
@@ -0,0 +1,17 @@
+/*
+ * SymbolTabelle.h
+ *
+ *  Created on: 19.03.2012
+ *      Author: stefan
+ */
+
+#ifndef SYMBOLTABELLE_H_
+#define SYMBOLTABELLE_H_
+
+class SymbolTabelle {
+public:
+	SymbolTabelle();
+	virtual ~SymbolTabelle();
+};
+
+#endif /* SYMBOLTABELLE_H_ */
