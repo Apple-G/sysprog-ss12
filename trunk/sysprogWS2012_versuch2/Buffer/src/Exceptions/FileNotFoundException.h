7897875550f214da04fc0461968a0268a31851d0
TobiasGiesel@gmail.com <TobiasGiesel@gmail.com@ec6a0c40-3177-3cdb-8e68-a0f64a91629d>
2012-10-11 18:25:04 +0000

    BRANCHED Ordnerstruktur entfernt

---
+++ trunk/sysprogWS2012_versuch2/Buffer/src/Exceptions/FileNotFoundException.h
@@ -0,0 +1,20 @@
+#include "InputOutputException.h"
+#include <iostream>
+
+class FileNotFoundException : public InputOutputException {
+       static const char* MESSAGE = "The file '%s' could not be found.";
+       char* completeMessage;
+
+public:
+        FileNotFoundException(char* filename) {
+            sprintf(completeMessage, MESSAGE, filename);
+        }
+
+        ~FileNotFoundException() {
+                delete[] completeMessage;
+        }
+
+        virtual const char* what() const throw() {
+                return this->completeMessage;
+        }
+};
