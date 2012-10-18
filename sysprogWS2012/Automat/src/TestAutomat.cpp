2c9c41b8680cf60420d755602695f96c18070fa4
stefan.kraus05@gmail.com <stefan.kraus05@gmail.com@ec6a0c40-3177-3cdb-8e68-a0f64a91629d>
2012-10-18 22:41:52 +0000

    automat makefile korrigiert shared librarys working wenn in konsole ausgeführt mit LD_LIBRARY_PATH

--- sysprogWS2012/Automat/src/TestAutomat.cpp
+++ sysprogWS2012/Automat/src/TestAutomat.cpp
@@ -1,9 +1,31 @@
 #include "Automat.h"
+#include "Token.h"
+#include "Buffer.h"
+#include "OutBuffer_OutputFileHandler.h"
+#include "OutBuffer_OutConsoleHandler.h"
+
 
 int main (int argc, char* argv[]){
 
 	Automat* automat;
 
-	automat = new Automat();
 
+	Buffer *b = new Buffer("TestFiles/test.txt",
+			"TestFiles/out.txt", 10);
+
+	//Handler
+
+	OutputHandlerBase* outfile = new OutputFileHandler(
+			"TestFiles/out.txt");
+	b->RegisterMessageHandler(outfile);
+
+	OutputHandlerBase* outConsole = new OutConsoleHandler();
+	b->RegisterMessageHandler(outConsole);
+
+	automat = new Automat(b);
+	Token t;
+	while(!b->isEOF()){
+		t=automat->nextToken();
+		std::cout<<t.getValue() << std::endl;
+	}
 }
--- sysprogWS2012/Scanner/src/TestScanner.cpp
+++ sysprogWS2012/Scanner/src/TestScanner.cpp
@@ -1,9 +1,11 @@
 #include "Scanner.h"
+//#include "Buffer.h"
 
 int main(int argc, char **argv) {
 
 	Scanner* scanner;
-
+//	Buffer *b;
+//	b=new Buffer();
 	scanner = new Scanner();
 
 }
