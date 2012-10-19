7953ac85e6e268f2043ac9aa2b455d718cd1107c
stefan.kraus05@gmail.com <stefan.kraus05@gmail.com@ec6a0c40-3177-3cdb-8e68-a0f64a91629d>
2012-10-19 18:48:08 +0000

    added scanner from old version.

--- sysprogWS2012/Scanner/src/Scanner.h
+++ sysprogWS2012/Scanner/src/Scanner.h
@@ -8,10 +8,25 @@
 #ifndef SCANNER_H_
 #define SCANNER_H_
 
-class Scanner {
+#include "Automat.h"
+#include "BufferIO.h"
+#include "Hashtable.h"
+#include "CharOperation.h"
+
+class Scanner
+{
+private:
+	BufferIO *buffer;
+	Automat *automat;
+	Hashtable <Token>*hashtable;
+
 public:
-	Scanner();
-	virtual ~Scanner();
+	Scanner(char*, char*);
+	virtual ~Scanner(void);
+//	void initSymbols();
+	void insert(char*, Token);
+	bool checkFile();
 };
+
 
 #endif /* SCANNER_H_ */
