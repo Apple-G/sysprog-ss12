793ebcf89e4b3f2a1cf996f8b587fea2d925cf0a
stefan.kraus05@gmail.com <stefan.kraus05@gmail.com@ec6a0c40-3177-3cdb-8e68-a0f64a91629d>
2012-10-28 19:17:27 +0000

    Scanner testet den Automaten und Buffer

--- sysprogWS2012/Scanner/src/TestScanner.cpp
+++ sysprogWS2012/Scanner/src/TestScanner.cpp
@@ -1,12 +1,21 @@
 #include "Scanner.h"
 //#include "Buffer.h"
 
-int main(int argc, char **argv) {
-
-	Scanner* scanner;
-//	Buffer *b;
-//	b=new Buffer();
-	scanner = new Scanner();
+//*******************************************************
+// Funktion:  main
+// Parameter: int argc, char* argv[
+// Return:    int
+// Notiz:     initialisiert den Scanner, und startet die Analyse.
+//*******************************************************
+int main(int argc, char* argv[]) {
+	if (argc < 1)
+	return EXIT_FAILURE;
+	Scanner* s = new Scanner("test.txt",
+			"out.txt");
+	if (s->checkFile()) {
+		return EXIT_SUCCESS;
+	} else {
+		return EXIT_FAILURE;
+	}
 
 }
-
