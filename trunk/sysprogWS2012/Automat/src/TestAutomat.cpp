4103f2c57bca4f114ac12b40d5a1fe117e68399d
ricbaral@gmail.com <ricbaral@gmail.com@ec6a0c40-3177-3cdb-8e68-a0f64a91629d>
2013-02-18 23:31:12 +0000

    Scanner/Automat funktioniert Tokenformatierung (leerzeichen)

--- trunk/sysprogWS2012/Automat/src/TestAutomat.cpp
+++ trunk/sysprogWS2012/Automat/src/TestAutomat.cpp
@@ -16,7 +16,7 @@ int main (int argc, char* argv[])
 	clock_t begin = clock();
 
 	Symboltable* symboltable = new Symboltable();
-	Buffer* buffer = new Buffer("TestFiles/bible.txt");
+	Buffer* buffer = new Buffer("TestFiles/newTest2.txt");
 	Automat* automat = new Automat(buffer);
 
 	//Handler
@@ -31,37 +31,73 @@ int main (int argc, char* argv[])
 
 	Token token;
 //TODO: error auf console, rest in datei
-//TODO tokentyp aus Symboltabelle lesen
+
 	while(!buffer->isEOF()){
 		token = automat->nextToken();
-		// std::cout<<(t.getLexem())<<std::endl;
+
 		if (token.getLexem()[0] != '\0')
 		{
-			buffer->writeMessage(CharHelper::convertInt(symboltable->insert(token.getLexem(), token.getType())));
-
-			buffer->writeMessage("\tToken ");
-			buffer->writeMessage(token.getTypeForOutput());
-			int count=0;
-			char* tempTokenType = token.getTypeForOutput();
-			while (*tempTokenType != 0)
+			if (token.getType() == Token::IDENTIFIER)
 			{
-				count++;
-				tempTokenType++;
+				buffer->writeMessage("Token ");
+				SymboltableEntry* entry = symboltable->insert(token.getLexem(), token.getType());
+				char* tokenType = Token::getTypeForOutput(entry->getTokenType());
+				buffer->writeMessage(tokenType);
+
+				buffer->writeMessage(" \t Line: ");
+				buffer->writeMessage(CharHelper::convertInt(token.getRow()));
+				buffer->writeMessage(" \t Column: ");
+				buffer->writeMessage(CharHelper::convertInt(token.getColumn()));
+				buffer->writeMessage(" \t Lexem: ");
+				buffer->writeMessage(token.getLexem());
+				buffer->writeMessage("\n");
+			}
+			else if (token.getType() == Token::INTEGER)
+			{
+				buffer->writeMessage("Token ");
+				char* tokenType = Token::getTypeForOutput(token.getType());
+				buffer->writeMessage(tokenType);
+
+				buffer->writeMessage(" \t Line: ");
+				buffer->writeMessage(CharHelper::convertInt(token.getRow()));
+				buffer->writeMessage(" \t Column: ");
+				buffer->writeMessage(CharHelper::convertInt(token.getColumn()));
+				buffer->writeMessage(" \t Value: ");
+				buffer->writeMessage(CharHelper::convertLong(token.getNumber()));
+				buffer->writeMessage("\n");
+			}
+			else if (token.getType() == Token::UNKNOWN)
+			{
+				buffer->writeError("Token ");
+				char* tokenType = Token::getTypeForOutput(token.getType());
+				buffer->writeError(tokenType);
+
+				buffer->writeError(" \t Line: ");
+				buffer->writeError(CharHelper::convertInt(token.getRow()));
+				buffer->writeError(" \t Column: ");
+				buffer->writeError(CharHelper::convertInt(token.getColumn()));
+				buffer->writeError(" \t Symbol: ");
+				buffer->writeError(token.getLexem());
+				buffer->writeError("\n");
+			}
+			else
+			{
+				buffer->writeMessage("Token ");
+				char* tokenType = Token::getTypeForOutput(token.getType());
+				buffer->writeMessage(tokenType);
+
+				buffer->writeMessage(" \t Line: ");
+				buffer->writeMessage(CharHelper::convertInt(token.getRow()));
+				buffer->writeMessage(" \t Column: ");
+				buffer->writeMessage(CharHelper::convertInt(token.getColumn()));
+				buffer->writeMessage("\n");
 			}
 
-			if (count<8)
-				buffer->writeMessage("\t");
 
-			buffer->writeMessage(" \t Line: ");
-			buffer->writeMessage(CharHelper::convertInt(token.getRow()));
-			buffer->writeMessage(" \t Column: ");
-			buffer->writeMessage(CharHelper::convertInt(token.getColumn()));
-			buffer->writeMessage(" \t Lexem: ");
-			buffer->writeMessage(token.getLexem());
-			buffer->writeMessage("\n");
+
 		}
-	}
 
+	}
 	clock_t end = clock();
 	// Measuring Time
 	// ======================================================================================================================================
