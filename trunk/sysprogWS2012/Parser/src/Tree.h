24258e40d6580c5b51da30aa7af8521ef3b042a6
TobiasGiesel@gmail.com <TobiasGiesel@gmail.com@ec6a0c40-3177-3cdb-8e68-a0f64a91629d>
2013-02-20 12:12:46 +0000

    INIT: Parser

---
+++ trunk/sysprogWS2012/Parser/src/Tree.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "../../Nodes/NodeProg.h"
+#include "../../Buffer/OutputBuffer.h"
+
+using namespace std;
+
+class Tree {
+	/** Zeiger auf den ersten Knoten des Baums. */
+	NodeProg *root;
+
+public:
+	/** Erzeugt einen neuen Baum. */
+	Tree(void);
+
+	/** Loescht den Baum und gibt beanspruchten Heap-Speicher wieder frei. */
+	~Tree(void);
+
+	/** .
+	@param node Ausgangsknoten des Baums, entspricht Programmeinstiegspunkt.
+	*/
+	void setRoot(NodeProg *node);
+
+	/** .
+	@param
+	@return
+	*/
+	NodeProg *getRoot();
+
+	/** Gibt den Baum rekursiv in verstaendlicher Sprache aus. */
+	void dump();
+
+	/** Fuehrt fuer den erzeugten Baum eine Typpruefung durch.
+	@param output Zeiger auf den zu verwendenden Ausgabepuffer.
+	@return Gibt zurueck, ob die Typpruefung erfolgreich war.
+	*/
+	bool typeCheck(OutputBuffer *output);
+
+	/** Erzeugt Code aus der Baumstruktur.
+	@param output Zeiger auf das zu verwendende Ausgabe-Interface.
+	*/
+	void makeCode(OutputBuffer *output);
+};
+
+
+
